d56bd59d77239abb894450d14fa06239d327f411
Artur Baumeister <Artur.baumeister@gmail.com>
2022-07-18 00:54:11 +0200

    Update path resolving for  .active class  Navlink

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,7 +1,7 @@
 // Using the Link component from react-router-dom to 
 // replace all anchor <a href="url"> tags with <Link to="url">
 // so we can reload and render only the Navlink components
-import { Link } from "react-router-dom";
+import { Link, useMatch, useResolvedPath } from "react-router-dom";
 export default function Navbar() {
   return (
     <nav className="nav">
@@ -19,11 +19,12 @@ export default function Navbar() {
 }
 /* When modifying use <Link> instead of <a> make sure to use everywhere "to" instead of "href" */
 function Navlink({ to, children, ...props }) {
-  const path = window.location.pathname;
-
-  // If the path is equal to href(to), set .active class to Navlink component.
+  //const path = window.location.pathname;
+  const resolvedPath = useResolvedPath(to);
+  const isActive = useMatch({ path: resolvedPath.pathname, end: true });
+  // If the entire url path is equal to href(to), set the var isActive to true, then set .active class to Navlink component.
   return (
-    <li className={path === to ? "active" : "" }>
+    <li className={isActive ? "active" : "" }>
       <Link to={to } {...props }>
         {children }
       </Link>
