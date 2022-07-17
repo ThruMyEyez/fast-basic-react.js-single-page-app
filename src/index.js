ba5f44fc8a36b5f1579c8cc05415ef95b00c1bab
Artur Baumeister <Artur.baumeister@gmail.com>
2022-07-17 23:56:08 +0200

    Adding BrowserRouter from react-router-dom

--- src/index.js
+++ src/index.js
@@ -2,17 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './style.css';
 import App from './App';
-//import reportWebVitals from './reportWebVitals';
+//first run "npm i react-router-dom" for BrowserRouter
+import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+root.render( //Wrap the whole WebApp with BrowserRouter
   <React.StrictMode>
-    <App />
+    <BrowserRouter> 
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-
-// reportWebVitals();
+// * import reportWebVitals from './reportWebVitals';
+// * and call it with:
+// * reportWebVitals();
