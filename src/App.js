5ffc2a3aeade8a2d073a84267142ff59e0de3b9e
Artur Baumeister <Artur.baumeister@gmail.com>
2022-07-17 21:00:18 +0200

    Add a switch function for routing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,41 @@
 //import logo from './logo.svg';
 //import './App.css';
 import Navbar from "./Navbar";
+import Home from "./pages/Home.js";
+import About from "./pages/About.js";
+import Blog from "./pages/Blog.js";
+import Downloads from "./pages/Downloads.js";
+import Pricing from "./pages/Pricing.js";
 
 function App() {
-  console.log(`Everything is fine now...`);
-  return <Navbar />
+  //It is important to use a Capitalized letter for the Var.
+  let component;
+
+  switch (window.location.pathname) {
+    case "/":
+      component = <Home />;
+      break;
+    case "/about":
+      component = <About />;
+      break;
+    case "/blog":
+      component = <Blog />;
+      break;
+    case "/downloads":
+      component = <Downloads />;
+      break;
+    case "/pricing":
+      component = <Pricing />;
+      break;
+    default:
+      break;
+  }
+  return (
+    <>
+      <Navbar />
+      {component}
+    </>
+  );
 }
 
 
