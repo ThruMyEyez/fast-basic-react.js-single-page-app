44ef88e8d496cf5279f65b4d6bf755fd833faa6a
Artur Baumeister <Artur.baumeister@gmail.com>
2022-07-17 23:23:18 +0200

    Adding Navlink() component

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,19 +1,25 @@
 export default function Navbar() {
-  return <nav className="nav">
-    <a href="/" className="site-title">MyPage</a>
-    <ul>
-      <li>
-        <a href="/about">About</a>
-      </li>
-      <li>
-        <a href="/blog">Articles</a>
-      </li>
-      <li>
-        <a href="/downloads">Downloads</a>
-      </li>
-      <li>
-        <a href="/pricing">Pricing</a>
-      </li>
-    </ul>
-  </nav>
+  return (
+    <nav className="nav">
+      <a href="/" className="site-title">
+        MyPage
+      </a>
+      <ul>
+        <Navlink href="/about">About</Navlink>
+        <Navlink href="/blog">Articles</Navlink>
+        <Navlink href="/downloads">Downloads</Navlink>
+        <Navlink href="/pricing">Pricing</Navlink>
+      </ul>
+    </nav>
+  );
+}
+
+function Navlink({ href, children, ...props }) {
+  const path = window.location.pathname;
+  // If the path is equal to href, set .active class to Navlink Comp
+  return (
+    <li className={path === href ? "active" : ""}> 
+      <a href={href} {...props}>{children}</a>
+    </li>
+  );
 }
